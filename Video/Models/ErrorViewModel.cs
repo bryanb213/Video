240ae7a53550b5ea1c6b83621898632127346f3b
Bryan Blancas <Bryan Blancas@DESKTOP-3L3C63I>
2020-06-15 12:53:41 -0500

    Add project files.

---
+++ Video/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Video.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
