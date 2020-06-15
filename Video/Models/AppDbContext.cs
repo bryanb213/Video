eeb4679ef448196dfe9aed2027de3025dbd8b3fd
Bryan Blancas <Bryan Blancas@DESKTOP-3L3C63I>
2020-06-15 15:22:58 -0500

    added customer controller, view, and model

---
+++ Video/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Video.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Video/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Video.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+
+        public byte NumberAvailable { get; set; }
+    }
+}
