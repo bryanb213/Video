5c069de3ad3017816843d1ada7db9d8003b5fd77
Bryan Blancas <Bryan Blancas@DESKTOP-3L3C63I>
2020-06-17 17:51:38 -0500

    added Get requests

--- Video/Controllers/MoviesController.cs
+++ Video/Controllers/MoviesController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Video.Models;
 
 namespace Video.Controllers
 {
     public class MoviesController : Controller
     {
+        private AppDbContext dbContext;
+
+        public MoviesController(AppDbContext context)
+        {
+            dbContext = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var movies = dbContext.Movies.ToList();
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = dbContext.Movies.SingleOrDefault(m => m.Id == id);
+            return View(movie);
         }
     }
 }
