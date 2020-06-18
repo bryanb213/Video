a99337d418f6a923096d5da692cf1b805580083f
Bryan Blancas <Bryan Blancas@DESKTOP-3L3C63I>
2020-06-18 14:40:22 -0500

    edited movie form

--- Video/Controllers/MoviesController.cs
+++ Video/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Video.Models;
+using Video.ViewModels;
 
 namespace Video.Controllers
 {
@@ -25,6 +26,45 @@ namespace Video.Controllers
         {
             var movie = dbContext.Movies.SingleOrDefault(m => m.Id == id);
             return View(movie);
+        }
+
+        public ActionResult New()
+        {
+            var viewModel = new NewMovieFormViewModel
+            {
+                Movie = new Movie()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Movie movie)
+        {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new NewMovieFormViewModel
+                {
+                    Movie = movie
+                };
+                return View("MovieForm", viewModel);
+            }
+            if(movie.Id == 0)
+            {
+                dbContext.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = dbContext.Movies.Single(x => x.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.NumberAvailable = movie.NumberAvailable;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+            dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
     }
 }
---
+++ Video/ViewModels/NewMovieFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Video.Models;
+
+namespace Video.ViewModels
+{
+    public class NewMovieFormViewModel
+    {
+        public Movie Movie { get; set; }
+    }
+}
